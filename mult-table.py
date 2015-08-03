7f0dacccc36ff7b41d713e6ee711dcdaa0ef4dc4
Scott <orthodarkness@gmail.com>
2015-08-03 18:18:14 -0400

    Commit

--- mult-table.py
+++ mult-table.py
@@ -1,5 +1,8 @@
-for row in range(1,13):
-    for col in range(1,13):
+columns = int(input("How many columns?:"))
+rows = int(input("How many rows?:"))
+
+for row in range(1,(rows + 1)):
+    for col in range(1,(columns + 1)):
         length = len(str(col*row))
         space = ' ' * (4 - length)
         print(row*col , space , end = '')
